4c72d3e17e307edf26824bf3b13f45832c9ebe52
SourangshuKundu723 <kundusourangshu@gmail.com>
2025-12-23 23:19:29 +0530

    Initial project setup

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+main().then((res) => {
+    console.log("connection successful");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/Wanderlust");
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+    res.send("root is working");
+});
+
+// app.get("/testListing", async (req, res) => {
+//     let listing = new Listing({
+//         title: "My new villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Goa",
+//         country: "India"
+//     });
+//     await listing.save();
+//     console.log("Sample is saved to DB");
+//     res.send("Successful!");
+// });
+
+//Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+app.listen(8080, () => {
+    console.log("app is listening to port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String,
+        default: "https://drive.google.com/file/d/1JoTmlTNnQ4GFaXNfg0WeC9rUIxe0zqBD/view?usp=sharing",
+        set: (v) => v === "" ? "https://drive.google.com/file/d/1JoTmlTNnQ4GFaXNfg0WeC9rUIxe0zqBD/view?usp=sharing" : v
+    },
+    price: {
+        type: Number
+    },
+    location: {
+        type: String
+    },
+    country: {
+        type: String
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;

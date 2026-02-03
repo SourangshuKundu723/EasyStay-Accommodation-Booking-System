be4b36daee065c7279f21d3c0f386f5a22d90284
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-02-03 13:54:16 +0530

    Add delete route for reviews

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //reviews
-//create route
+//Create review route
 app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
     let {id} = req.params;
     let listing = await Listing.findById(id);
@@ -125,6 +125,14 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => 
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
+    res.redirect(`/listings/${id}`);
+}));
+
+//Delete review route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let {id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: {reviews: reviewId} });
+    await Review.findByIdAndDelete(reviewId);
     res.redirect(`/listings/${id}`);
 }));
 

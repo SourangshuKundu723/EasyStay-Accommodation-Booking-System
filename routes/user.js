19230b2fb65ddaf70f1314e22da3f314ee35831d
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-08-25 01:06:15 +0530

    Minor fixes

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const User = require("../models/user.js");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
+const { isLoggedIn, saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/users.js");
 
@@ -20,6 +20,6 @@ router.route("/login")
 
 router.get("/logout", userController.logout);
 
-router.get("/profile", userController.showProfile);
+router.get("/profile", isLoggedIn, userController.showProfile);
 
 module.exports = router;

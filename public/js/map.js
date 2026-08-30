87430598ff24a4fa8887f38224656145ac1be22a
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-08-30 14:01:24 +0530

    Added full screen control for map and enabled the scrollzoom

--- public/js/map.js
+++ public/js/map.js
@@ -3,18 +3,19 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
     projection: 'globe', // display the map as a globe
-    zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
+    zoom: 3, // initial zoom level, 0 is the world view, higher values zoom in
     center: coordinates // center the map on this longitude and latitude
 });
 
 map.addControl(new mapboxgl.NavigationControl());
-map.scrollZoom.disable();
 
 map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
 });
 
-const popup = new mapboxgl.Popup({ offset: 25 })
+map.addControl(new mapboxgl.FullscreenControl());
+
+const popup = new mapboxgl.Popup({ offset: 25, draggable: true })
     .setHTML("<p>Exact location provided after booking</p>");
 
 //Map marker
@@ -22,3 +23,11 @@ const marker = new mapboxgl.Marker({color: "red"})
     .setLngLat(coordinates)
     .setPopup(popup)
     .addTo(map);
+
+map.flyTo({
+    center: coordinates,
+    zoom: 9,
+    speed: 1.1,
+    curve: 1.1,
+    essential: true
+});

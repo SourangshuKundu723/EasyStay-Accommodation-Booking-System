a1fe3270e381bd8a086cc05b613e0ed198de9691
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-08-30 13:40:40 +0530

    Added map functionalities using mapbox-sdk and updated listing schema to store coordinates of the location

--- public/js/map.js
+++ public/js/map.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
     projection: 'globe', // display the map as a globe
     zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
-    center: [88.3639, 22.5726] // center the map on this longitude and latitude
+    center: coordinates // center the map on this longitude and latitude
 });
 
 map.addControl(new mapboxgl.NavigationControl());
@@ -13,3 +13,12 @@ map.scrollZoom.disable();
 map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
 });
+
+const popup = new mapboxgl.Popup({ offset: 25 })
+    .setHTML("<p>Exact location provided after booking</p>");
+
+//Map marker
+const marker = new mapboxgl.Marker({color: "red"})
+    .setLngLat(coordinates)
+    .setPopup(popup)
+    .addTo(map);
